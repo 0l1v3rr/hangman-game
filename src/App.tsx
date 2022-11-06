2f89180401671bade56730ae3317a3269aaee4c1
Olivér <oliver.mrakovics@gmail.com>
2022-11-06 15:14:39 +0100

    added a custom hook to manually re-render the components

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useCallback, useRef, useState } from "react";
 import Hangman from "./components/game/Hangman";
 import KeyboardSection from "./components/game/KeyboardSection";
 import Header from "./components/Header";
+import { useForceRerender } from "./components/hooks/useForceRerender";
 import Navbar from "./components/Navbar";
 import HangmanGame from "./hangman";
 import words from "./words.json";
@@ -11,13 +12,26 @@ const getRandomWord = (): string => {
 };
 
 const App = () => {
+  const forceRerender = useForceRerender();
+
   const [hints, setHints] = useState<number>(3);
   const [game, setGame] = useState<HangmanGame>(
     new HangmanGame(getRandomWord())
   );
 
   const guessLetter = useCallback(
-    (letter: string): void => game.guessLetter(letter),
+    (letter: string): void => {
+      if (game.phase >= 6) {
+        return;
+      }
+
+      if (game.isGuessed()) {
+        return;
+      }
+
+      game.guessLetter(letter);
+      forceRerender();
+    },
     [game]
   );
 
@@ -44,6 +58,7 @@ const App = () => {
     }
 
     game.giveUp();
+    forceRerender();
   }, [game]);
 
   return (
